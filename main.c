81bce3c8eb489dce4330e05f0edeccdd3a4b6038
JoaoPMenegaz <102390977+JoaoPMenegaz@users.noreply.github.com>
2022-04-04 23:19:19 -0300

    corrigindo conforme o enunciado
    corrigi a forma de entrada dos dados, que na primeira vez por desatenção fiz errado.

--- main.c
+++ main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
-char funcao (char *matriz[]){
+char decodificacao (char *matriz[],int count,char resposta[])
+{
     char a,b;
     int i,j;
-printf("digite o codigo #(numero)=(vezes) ");
-scanf(" %c%d%c%d", &a, &i, &b, &j);
-printf("a letra foi %c\n",matriz[i][j]);
-
-return matriz[i][j];
-}
-
-
-
-int main (){
     
-char *matriz[10] = {"",""," abc"," def"," ghi"," jkl"," mno"," pqrs"," tuv"," wxyz"};
-
-while(1){
-funcao(matriz);
+    printf("digite os %d caracteres no formato #(numero)=(vezes) \n",count+1);
+        for (int k=0; k<=count; k++){
+            scanf(" %c%d%c%d", &a, &i, &b, &j);
+            resposta[k]=matriz[i][j];
+        }
 }
+
+int main ()
+{
+    int count=0;   
+    char *matriz[10] = {"",""," abc"," def"," ghi"," jkl"," mno"," pqrs"," tuv"," wxyz"};
+
+    printf("digite o numero de caracteres a serem decodificador ");
+    scanf(" %d",&count);
+    count=count-1;
+    char resposta[count];
+
+    decodificacao(matriz,count,resposta);
+
+    printf("\n%s",resposta);
 
 return(0);
 }
